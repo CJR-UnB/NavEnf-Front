9ed0f678ddad96ee181f883ea6deb63605892704
Matheus Barbosa <me.matheusbarbosa@proton.me>
2026-09-11 19:09:20 -0300

    feature(fonts): adicionado fontes e estilos de fonts

--- navenf/app/layout.tsx
+++ navenf/app/layout.tsx
@@ -2,6 +2,17 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import NavBar from './components/Navbar';
+import localFont from "next/font/local";
+
+const atkinson = localFont({
+  src: "./fonts/AtkinsonHyperlegible-Regular.ttf",
+  variable: "--font-atkinson",
+});
+
+const calSans = localFont({
+  src: "./fonts/CalSans-Regular.ttf",
+  variable: "--font-cal-sans",
+});
 
 const geistSans = Geist({
 	variable: '--font-geist-sans',
@@ -26,7 +37,7 @@ export default function RootLayout({
 	return (
 		<html
 			lang="pt-BR"
-			className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+			className={`${geistSans.variable} ${geistMono.variable} ${atkinson.variable} ${calSans.variable} h-full antialiased`}
 		>
 			<body className="min-h-full flex flex-col">
 				<NavBar />
